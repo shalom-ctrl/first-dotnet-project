68df97277b0d7cc0b8c85301c37062f203c396e0
Shalom Achikasim <shalomachims@gmail.com>
2026-04-10 13:26:25 +0100

    Implement Create Operation on Employee Database

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,43 @@
+﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Models.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //localhost:xxxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee()
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary,
+            };
+
+            dbContext.Employees.Add(employeeEntity);
+            dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
+        }
+
+
+
+    }
+}
